d512b54226c7ed1a38c851e8da46578cf0dda9ce
Mateus Moraes <44010175+h4rdrew@users.noreply.github.com>
2020-08-07 13:36:04 -0300

    Teste 2
    Adicionado movimentação para o player e fisica de colisão

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log($"Batemos em um {collisionInfo.collider.name}");
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // We marked this as "Fixed"Update because we
+    // are using it to mess with physics
+    void FixedUpdate()
+    {
+        //rb.useGravity = false;
+        //rb.AddForce(0, 200, 500);
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
